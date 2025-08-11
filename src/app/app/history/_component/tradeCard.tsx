23827fd397b3a44d427a1d617f568c72023977a4
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2025-08-11 18:35:46 +0530

    Trade bug Fixd

--- src/app/app/history/_component/tradeCard.tsx
+++ src/app/app/history/_component/tradeCard.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+interface TradeData {
+  id: string;
+  userId: string;
+  openingBalance: number;
+  closingBalance: number;
+  usedBalance: number;
+  type: string;
+  status: string;
+  createdAt: Date;
+}
 
-
-const TradeCard = ({ TradaData }) => {
+const TradeCard = ({ TradaData }: { TradaData: TradeData }) => {
   return (
     <div className="border p-4 rounded-lg shadow-md bg-primary hover:shadow-lg transition-shadow duration-300">
       <div className="grid grid-cols-2">
-        <div></div>
+        <div>{TradaData.type}</div>
       </div>
     </div>
   );
